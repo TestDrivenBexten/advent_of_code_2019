e57c2b5e2f6fefd864929acbf138976c5f9426d5
Skyler Bexten <skyler.bexten@gmail.com>
2021-11-29 20:36:20 -0700

    Pass first input

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,4 @@
+
+fun calculateFuel(mass: Int): Int {
+    return (mass / 3) - 2
+}
---
+++ src/test/kotlin/Day1Tests.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Day 1 Tests")
+class Day1Tests {
+
+    @Test
+    @DisplayName("Should require 2 fuel for mass of 12")
+    fun should_require_2_fuel_for_mass_12(){
+        val fuel = calculateFuel(12)
+        assertEquals(2, fuel)
+    }
+}
